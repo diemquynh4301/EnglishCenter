a3ee350f76b59f3d148e52de84ea7b28466ce845
diemquynh4301 <quynh.ngo4301@hcmut.edu.vn>
2021-11-26 15:25:54 +0700

    login/index file

--- src/modules/admin/user.controller.ts
+++ src/modules/admin/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post, Render, Res } from "@nestjs/common";
 import { User } from "src/models/user.entity";
 import { UserService } from './../../service/user.service';
+import { Request, Response } from 'express';
 import * as bcrypt from 'bcrypt'; 
 
 @Controller('users')
@@ -15,11 +16,10 @@ export class UserController{
         }
     }
     @Post("add")
-    async add(@Body() user: User) {
-    //async add(@Body() user: User, @Res() res: Response) {
+    async add(@Body() user: User, @Res() res: Response) {
         // const salt = await bcrypt.genSalt(15);
         // user.pass = await bcrypt.hash(user.pass, salt);
         await this.userService.add(user);
-        //return res.redirect("/course");        
+        return res.redirect('/course');        
     }
 }
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,18 +1,28 @@
-import { Request } from 'express';
-import { Controller, Get, Render, UseGuards, Post, Req } from "@nestjs/common";
+import { Controller, Get, Render, UseGuards, Post, Req, Res, Body} from "@nestjs/common";
+import { Request, Response } from 'express';
 import { AuthGuard } from "@nestjs/passport";
+import { UserService } from "src/service/user.service";
+import { User } from "src/models/user.entity";
 
-@Controller()
+@Controller('login')
 export class AuthController {
-    @Get('login')
+    constructor(private userService: UserService) {}
+    @Get()
     @Render("login/index")
     async loginPage() {
 
     }
 
+    @Get('/add')
+    @Render("login/add")
+    async add(@Body() user: User, @Res() res: Response) {
+        await this.userService.add(user);
+        return res.redirect('/course');   
+    }
+
     @Post('login')
     @UseGuards(AuthGuard('local'))
-    login(@Req() req: Request) {
-        return 'success';
+    login(@Req() req: Request, @Res() res: Response) {
+        return res.redirect('/course');
     }
 }
